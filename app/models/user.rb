2fd8b5c50594bdf49f1228f47eb38abc4ff8c362
PinappleAndApplePen <sachie.thornton@gmail.com>
2023-08-11 10:19:10 +0900

    新規登録保存完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-                  # app/models/user.rb
-
-  validates :nickname, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :family_name_zenkaku, presence: true
-  validates :first_name_zenkaku, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :date_of_birth, presence: true
-  
+    
+         validates :nickname,presence: true
+         validates :email,presence: true
+         validates :nickname, presence: true
+         validates :family_name_zenkaku, presence: true
+         validates :first_name_zenkaku, presence: true
+         validates :family_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :date_of_birth, presence: true
+         
+    
 end
