b64a49bcd58e30b22748d3033d2685b84f486607
PinappleAndApplePen <sachie.thornton@gmail.com>
2023-08-13 21:18:23 +0900

    任意修正完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     nickname              { Faker::Name.initials(number: 2)}
     email                 { Faker::Internet.email}
     password              { 'ay5i69' } 
-    password_confirmation {'ay5i69' }
+    password_confirmation { password }
     family_name_zenkaku { '山田' }
     first_name_zenkaku { '陸太郎' }
     family_name_kana { 'ヤマダ' }
