42ebba1c00aa16294759cf5cecce844953ae770b
PinappleAndApplePen <sachie.thornton@gmail.com>
2023-08-18 14:44:15 +0900

    削除ボタン完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only:[:edit, :destory]
   #before_action :move_to_index, except: [:index, :show]
   
   def index
@@ -10,6 +12,30 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new # Item出品画面へ遷移するnewアクションのコード
   end
+
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
 
 
   def create
@@ -40,17 +66,19 @@ class ItemsController < ApplicationController
       ).merge(user_id: current_user.id)
   end
 
-  def show
+   
+
+
+  def contributor_confirmation
+     redirect_to root_path unless  current_user == @item.user
+  end
+
+   
+
+  def set_item
     @item = Item.find(params[:id])
   end
 
- 
-  #def destroy
-   #item = Item.find(params[:id])
-   #item.destroy
-   #redirect_to root_path
-  #end
-
-
-
 end
+
+
