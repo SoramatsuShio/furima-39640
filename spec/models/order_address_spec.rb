796c875e41a448c01f3e853defb93ccba932f915
PinappleAndApplePen <sachie.thornton@gmail.com>
2023-08-21 21:26:50 +0900

    単体テスト（異常系）

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
+
 RSpec.describe OrderAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, item_id: item.id, user_id: user.id)
+  end
+
   describe '商品購入登録' do
-    before do
-      user = FactoryBot.create(:user)
-      item = FactoryBot.create(:item)
-      @order_address = FactoryBot.build(:order_address, item_id: item.id, user_id: user.id)
-    end
+  
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_address).to be_valid
@@ -14,7 +17,22 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.building_name = ''
         expect(@order_address).to be_valid
       end
+    end
+
+     context '内容に問題がある場合' do
+      
+      it 'zipcodeが空だと保存できないこと' do
+        @order_address.zipcode = ''
+        @order_address.valid?
+        binding.pry
+        expect(@order_address.errors.full_messages).to include('Zipcode can't be blank, Zipcode is invalid. Include hyphen(-)')
+      end
+
+           
+
      
+
+
     end
    
   end
