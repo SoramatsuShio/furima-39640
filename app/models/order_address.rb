cf34080e0b07f6e1bde9c2d289495991cc6cf1b5
PinappleAndApplePen <sachie.thornton@gmail.com>
2023-08-21 23:01:20 +0900

    単体テスト異常系Token以外は終了

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,12 +8,17 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :zipcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :shipping_origin_id
+    validates :shipping_origin_id, numericality: { other_than: 1 , message: "は---以外で入力してください"}
     validates :city
     validates :street_address
-    validates :phone
+    validates :phone, format: {with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値のみで入力してください"}
    end
     
+
+
+
+
+
   def save
     # 商品情報を保存し、変数orderに代入する
     order = Order.create(user_id: user_id ,item_id: item_id)
